624758da2ea9e2b2e427b2b594d5067025fc161b
Jeffrey <jefferyaboh@gmail.com>
2020-02-22 20:31:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# myethion.com
---
+++ livezilla/_config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+/*********************************************************************************
+* LiveZilla config.php
+*
+* Copyright 2016 LiveZilla GmbH
+* All rights reserved.
+* LiveZilla is a registered trademark.
+*
+********************************************************************************/
+
+$_CONFIG['gl_pr_cr'] = '1545757652';
+$_CONFIG['gl_lzid'] = '8e18b20ef5';
+$_CONFIG['b64'] = false;
+
+$_CONFIG[0]['gl_db_host'] = 'localhost';
+$_CONFIG[0]['gl_db_user'] = 'adsproco_admin';
+$_CONFIG[0]['gl_db_ext'] = 'mysqli';
+$_CONFIG[0]['gl_db_eng'] = 'MyISAM';
+$_CONFIG[0]['gl_db_pass'] = 'Verifylogin1%';
+$_CONFIG[0]['gl_db_name'] = 'adsproco_livezilla';
+$_CONFIG[0]['gl_db_prefix'] = 'lz_';
+
+?>
